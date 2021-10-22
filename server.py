bf62a8d10770a4f5bfb0c14f92346082abfa8288
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-22 00:41:25 +0000

    Adding notes and requirements

---
+++ server.py
@@ -0,0 +1,27 @@
+import requests
+
+
+
+
+
+# Take in a list of tax form names
+
+# Search the website for the tax forms
+
+# Return: JSON of the "Product Number", the "Title", and the maximum and minimum 
+# years the form is available for download.
+
+# Return: download all PDFs available within the years range specified. The 
+# downloaded PDFs should be downloaded to a subdirectory under your script's main 
+# directory with the name of the form, and the file name should be the "Form Name - Year"
+
+# click (cli)
+# optional arg parse - stnd py package (cli)
+
+# parse website with bs3
+# to dl pdf file use python requests, remember to use flags
+
+#tax_form_names = ["Form W-2", "Form 1095-C"]
+
+
+
