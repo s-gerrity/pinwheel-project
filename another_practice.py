e2ce02a1c7d8f42cb347192da2a848b6ea7fdf59
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-25 06:25:08 +0000

    Added and commented out practice code for downloading pdfs

--- another_practice.py
+++ another_practice.py
@@ -29,8 +29,32 @@ def data_for_forms(tax_form_names):
 
         # object that takes page.content, which is the HTML content you scraped, as its input
         soup = BeautifulSoup(page.content, "html.parser")
-        links = soup.find_all('a')
-        print("Total Links Found:",links.__len__())
+
+        # links = soup.find_all('a')
+        # # for link in links:
+        # #     link_url = link["href"]
+        # #     if 'pdf' in link_url:
+        # #         print(link_url)
+        # i = 0
+        # for link in links[:23]:
+        #     if ('.pdf' in link.get('href', [])):
+        #         i += 1
+        #         print("Downloading file: ", i)
+  
+        # # Get response object for link
+        #         response = requests.get(link.get('href'))
+        #         print(link.get('href'))
+        #         pdf = open("pdf"+str(i)+".pdf", 'wb')
+        #         pdf.write(response.content)
+        #         pdf.close()
+        #         print("File ", i, " downloaded")
+  
+        # print("All PDF files downloaded")
+        # print(links.text.strip())
+        # filelink = link for link in links: if ('.pdf' in link.get('href')): print(link.get('href')) filelink = link.get('href') 
+        # print(filelink)
+        # break
+
 
         # this creats an "iterable" to loop through all results
         results = soup.find("div", class_="picklistTable")
