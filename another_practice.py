d0250d3b7893bfbc61c332d2373656d456b65ec7
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-25 05:07:33 +0000

    Lower case for url iteration; tested iteration for form search by adding to searched list tax_form_names

--- another_practice.py
+++ another_practice.py
@@ -5,7 +5,7 @@ import json
 from bs4 import BeautifulSoup
 
 # list of all tax forms we need to pull
-tax_form_names = ["Form W-2"]
+tax_form_names = ["Form W-2", "Form 1095-C"]
 dict_for_data = {}
 
 ##### HELPER FUNCTION
@@ -13,7 +13,7 @@ dict_for_data = {}
 def collect_all_form_years(all_form_years_list, form_data):
     for form in form_data:
         form_year = form.find("td", class_="EndCellSpacer")
-        all_form_years.append(form_year.text.strip())
+        all_form_years_list.append(form_year.text.strip())
 
     return all_form_years_list
 
@@ -23,53 +23,49 @@ def collect_all_form_years(all_form_years_list, form_data):
 
 # https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=25&sortColumn=sortOrder&value=Form+W-2&criteria=formNumber&resultsPerPage=25&isDescending=false
 # https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=Form+W-2&criteria=formNumber&resultsPerPage=25&isDescending=false
+def data_for_forms(tax_form_names):
+    # TODO: iterate through pages somehow
+    for form_to_check in tax_form_names:
+        new_URL = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=" + form_to_check.lower() + "&criteria=formNumber&resultsPerPage=25&isDescending=false"
 
-# TODO: iterate through pages somehow
-for form_to_check in tax_form_names:
-    new_URL = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=" + form_to_check + "&criteria=formNumber&resultsPerPage=25&isDescending=false"
+        page = requests.get(new_URL)
 
-    page = requests.get(new_URL)
+        # object that takes page.content, which is the HTML content you scraped, as its input
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    # object that takes page.content, which is the HTML content you scraped, as its input
-    soup = BeautifulSoup(page.content, "html.parser")
+        # this creats an "iterable" to loop through all results
+        results = soup.find("div", class_="picklistTable")
+        
+        # print(results.prettify())
 
-    # this creats an "iterable" to loop through all results
-    results = soup.find("div", class_="picklistTable")
-    
-    # print(results.prettify())
+        even_form_data = results.find_all("tr", class_="even")
+        odd_form_data = results.find_all("tr", class_="odd")
 
-    even_form_data = results.find_all("tr", class_="even")
-    odd_form_data = results.find_all("tr", class_="odd")
+        for form in even_form_data:
+            # print(form.prettify(), end="\n"*2)
+            checker = False
+            product_number = form.find("td", class_="LeftCellSpacer")
 
-    # this matches exact strings
-# python_jobs = results.find_all("h2", string="Python")
-# print(python_jobs)
+            if product_number.text.strip() == form_to_check and checker == False:
+                dict_for_data['Product Number'] = product_number.text.strip()
+                checker = True
+                form_title = form.find("td", class_="MiddleCellSpacer")
+                dict_for_data['Title'] = form_title.text.strip()
+        
+        all_form_years = []
 
-    for form in even_form_data:
-        # print(form.prettify(), end="\n"*2)
-        checker = False
-        product_number = form.find("td", class_="LeftCellSpacer")
+        # calls helper function to collect all the years the form is available
+        all_form_years_list = collect_all_form_years(all_form_years, even_form_data)
+        all_form_years_list = collect_all_form_years(all_form_years, odd_form_data)    
 
-        if product_number.text.strip() == form_to_check and checker == False:
-            dict_for_data['Product Number'] = product_number.text.strip()
-            checker = True
-            form_title = form.find("td", class_="MiddleCellSpacer")
-            dict_for_data['Title'] = form_title.text.strip()
-    
-    all_form_years = []
+        dict_for_data['Minimum Year'] = min(all_form_years)
+        dict_for_data['Maximum Year'] = max(all_form_years)
 
-    # calls helper function to collect all the years the form is available
-    all_form_years_list = collect_all_form_years(all_form_years, even_form_data)
-    all_form_years_list = collect_all_form_years(all_form_years, odd_form_data)    
-
-    dict_for_data['Minimum Year'] = min(all_form_years)
-    dict_for_data['Maximum Year'] = max(all_form_years)
-
-json_object = json.dumps(dict_for_data, indent = 4) 
-print(json_object)
+        json_object = json.dumps(dict_for_data, indent = 4) 
+        print(json_object)
 
 
 
 
-
+data_for_forms(tax_form_names)
 
