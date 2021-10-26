cfef07f6044b3a3bd5b5fc90d3814e65b73c99e7
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-26 23:35:45 +0000

    Separated out web scrap into a function; added pagination samples in comments

--- practice.py
+++ practice.py
@@ -1,18 +1,9 @@
 
 import requests
 import json
+import re
 from bs4 import BeautifulSoup
 
-# Take in a list of tax form names
-
-# Search the website for the tax forms
-
-# Return: JSON of the "Product Number", the "Title", and the maximum and minimum 
-# years the form is available for download.
-
-# Return: download all PDFs available within the years range specified. The 
-# downloaded PDFs should be downloaded to a subdirectory under your script's main 
-# directory with the name of the form, and the file name should be the "Form Name - Year"
 
 # click (cli)
 # optional arg parse - stnd py package (cli)
@@ -25,7 +16,60 @@ tax_forms_to_check = ["Form W-2", "Form 1095-C"]
 
 # TODO: pagenation
 
-def locate_data_on_page(soup):
+# url_last_page = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=200&sortColumn=sortOrder&value=Form+w-2&criteria=formNumber&resultsPerPage=200&isDescending=false"
+# url_not_last_page = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?resultsPerPage=200&sortColumn=sortOrder&indexOfFirstRow=0&criteria=formNumber&value=Form+w-2&isDescending=false"
+# base_url = "https://apps.irs.gov"
+# page_four = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=75&sortColumn=sortOrder&value=Form+w-2&criteria=formNumber&resultsPerPage=25&isDescending=false"
+
+# def start_checking():
+
+#     page = requests.get(url_not_last_page)
+
+#     soup = BeautifulSoup(page.content, "html.parser")
+
+#     results = soup.find("th", class_="NumPageViewed")
+
+#     pagenation_links = results.find_all("a")
+#     print(pagenation_links)
+#     print()
+#     for item in pagenation_links:
+#         if "Next" in item.text:
+#             new_url = base_url + item['href']
+#     return 
+
+
+# def go_to_next_page():
+#     # take in the url and change it. update index begins.
+
+#     # takes url
+#     page = requests.get(page_four)
+
+#     # gets all page content
+#     soup = BeautifulSoup(page.content, "html.parser")
+
+#     # finds on the page the link within "next", but without the base url
+#     next_page = soup.find('a', text=re.compile(r'.*Next.*'))
+#     if next_page == None:
+#         print("next page none")
+#         print()
+#     else:
+#         print(next_page['href'])
+#         print("new link: " + base_url + next_page['href'])
+
+
+# pseudocode
+# do we check for next after we've scraped the page already once?
+# if next in <a> tag
+# move to next page and keep adding to the same json set
+# add 200 to the urls start index on
+
+    
+
+def locate_data_on_page(url):
+
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
 
     # this creats an "iterable" to loop through all results
     results = soup.find("div", class_="picklistTable")
@@ -79,17 +123,28 @@ def append_to_main_list_as_json(dict_with_form_data, tax_form_info):
     return tax_form_info
 
 
+# def make_url():
+
+#     base_url = "x"
+
+
+
+# def scrape_page(new_url):
+#     url = new_url
+
+
+
+#     return url 
+
+
 def get_tax_info(tax_forms_to_check):
     tax_form_info = []
 
     for form_to_check in tax_forms_to_check:
+
         url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=" + form_to_check.lower() + "&criteria=formNumber&resultsPerPage=25&isDescending=false"
 
-        page = requests.get(url)
-
-        soup = BeautifulSoup(page.content, "html.parser")
-
-        form_data = locate_data_on_page(soup)
+        form_data = locate_data_on_page(url)
         dict_for_data = collect_tax_form_details(form_data, form_to_check)
         all_form_years = collect_tax_years(form_data, form_to_check)
         dict_with_form_data = get_min_max_years(all_form_years, dict_for_data)
@@ -101,3 +156,6 @@ def get_tax_info(tax_forms_to_check):
 
 
 get_tax_info(tax_forms_to_check)
+
+# start_checking()
+# go_to_next_page()
