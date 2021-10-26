df5f5e7f7d5b270763d90ff88bc57471157ca11c
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-26 19:25:47 +0000

    Practice sheet final return is json in a list

--- practice.py
+++ practice.py
@@ -22,6 +22,7 @@ from bs4 import BeautifulSoup
 
 tax_form_names = ["Form W-2", "Form 1095-C"]
 dict_for_data = {}
+tax_form_info = []
 
 # TODO: break function into smaller functions
 # make soup, grab data, loop through data to label key: value pairs, mix and max, 
@@ -72,50 +73,13 @@ def get_min_max_years(dict_for_data, all_form_years):
     dict_for_data['Minimum Year'] = min(all_form_years['Years'])
     dict_for_data['Maximum Year'] = max(all_form_years["Years"])
 
-    return dict_to_json(dict_for_data)
-
-def dict_to_json(dict_for_data):
-    json_object = json.dumps(dict_for_data, indent = 4) 
-    return json_object
+    return append_to_main_list_as_json(dict_for_data, tax_form_info)
 
 
-# def data_for_forms(tax_form_names):
-#     # TODO: iterate through pages somehow
-#     for form_to_check in tax_form_names:
-#         print(form_to_check, "form_to_check")
-#         new_URL = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=" + form_to_check.lower() + "&criteria=formNumber&resultsPerPage=25&isDescending=false"
+def append_to_main_list_as_json(json_tax_info, tax_form_info):
+    tax_form_info.append(json_tax_info)
 
-#         page = requests.get(new_URL)
-
-#         # object that takes page.content, which is the HTML content you scraped, as its input
-#         soup = BeautifulSoup(page.content, "html.parser")
-
-#         # this creats an "iterable" to loop through all results
-#         results = soup.find("div", class_="picklistTable")
-        
-#         form_data = results.find_all("tr", class_=["even", "odd"])
-#         all_form_years = []
-
-#         for form in form_data:
-#             product_number = form.find("td", class_="LeftCellSpacer")
-
-#             if product_number.text.strip() == form_to_check:
-#                 dict_for_data['Product Number'] = product_number.text.strip()
-#                 form_title = form.find("td", class_="MiddleCellSpacer")
-#                 form_year = form.find("td", class_="EndCellSpacer")
-                
-#                 # collect applicable years in a list to sort the min and max later
-#                 all_form_years.append(form_year.text.strip())
-#                 dict_for_data['Title'] = form_title.text.strip()        
-    
-#         dict_for_data['Minimum Year'] = min(all_form_years)
-#         dict_for_data['Maximum Year'] = max(all_form_years)
-
-#         json_object = json.dumps(dict_for_data, indent = 4) 
-#         return json_object
+    return json.dumps(tax_form_info, indent = 4)
 
 
-
-
-# print(data_for_forms(tax_form_names))
 print(get_page_html(tax_form_names))
