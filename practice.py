9fbfe5793baa649ba7f0a0d268ee26af3e94112d
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-26 21:32:18 +0000

    Functions work and produce correct json w iterations

--- practice.py
+++ practice.py
@@ -21,29 +21,22 @@ from bs4 import BeautifulSoup
 # to dl pdf file use python requests, remember to use flags
 
 tax_forms_to_check = ["Form W-2", "Form 1095-C"]
-dict_for_data = {}
-tax_form_info = []
-
-# TODO: break function into smaller functions
-# make soup, grab data, loop through data to label key: value pairs, mix and max, 
-# turn into json, pagenation
 
 
+# TODO: pagenation
 
-def locate_data_on_page(soup, form_to_check, tax_form_info):
+def locate_data_on_page(soup, tax_form_info):
 
     # this creats an "iterable" to loop through all results
     results = soup.find("div", class_="picklistTable")
 
     form_data = results.find_all("tr", class_=["even", "odd"])
-    print(form_to_check, "2")
 
-    return collect_tax_form_details(form_data, form_to_check, tax_form_info)
+    return form_data
 
 
 def collect_tax_form_details(form_data, form_to_check, tax_form_info):
-
-    all_form_years = {'Years': []}
+    dict_for_data = {}
 
     for form in form_data:
         product_number = form.find("td", class_="LeftCellSpacer")
@@ -51,48 +44,61 @@ def collect_tax_form_details(form_data, form_to_check, tax_form_info):
         if product_number.text.strip() == form_to_check:
             dict_for_data['Product Number'] = product_number.text.strip()
             form_title = form.find("td", class_="MiddleCellSpacer")
-            form_year = form.find("td", class_="EndCellSpacer")
-            
+
             # collect applicable years in a list to sort the min and max later
-            all_form_years['Years'].append(form_year.text.strip())
             dict_for_data['Title'] = form_title.text.strip()
-    # print(all_form_years)
-    # print(dict_for_data)
 
-    return get_min_max_years(dict_for_data, all_form_years, tax_form_info)
+    return dict_for_data
+
+def collect_tax_years(form_data, form_to_check, tax_form_info):
+    all_form_years = {'Years': []}
+
+    for form in form_data:
+            product_number = form.find("td", class_="LeftCellSpacer")
+
+            if product_number.text.strip() == form_to_check:
+                form_year = form.find("td", class_="EndCellSpacer")
+                all_form_years['Years'].append(form_year.text.strip())
 
 
-def get_min_max_years(dict_for_data, all_form_years, tax_form_info):
+    return all_form_years
+
+
+def get_min_max_years(all_form_years, dict_for_data, tax_form_info):
 
     dict_for_data['Minimum Year'] = min(all_form_years['Years'])
     dict_for_data['Maximum Year'] = max(all_form_years["Years"])
-    # print(dict_for_data)
 
-    return append_to_main_list_as_json(dict_for_data, tax_form_info)
+    return dict_for_data
 
 
-def append_to_main_list_as_json(dict_for_data, tax_form_info):
-    tax_form_info.append(dict_for_data)
-    print(tax_form_info)
+def append_to_main_list_as_json(dict_with_form_data, tax_form_info):
+
+    tax_form_info.append(dict_with_form_data)
 
     return tax_form_info
 
 
-def get_page_html(tax_forms_to_check):
+def get_tax_info(tax_forms_to_check):
+    tax_form_info = []
 
     for form_to_check in tax_forms_to_check:
-        print(form_to_check, "form_to_check")
         url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=" + form_to_check.lower() + "&criteria=formNumber&resultsPerPage=25&isDescending=false"
 
         page = requests.get(url)
 
         soup = BeautifulSoup(page.content, "html.parser")
 
-        json_tax_forms = locate_data_on_page(soup, form_to_check, tax_form_info)
+        form_data = locate_data_on_page(soup, tax_form_info)
+        dict_for_data = collect_tax_form_details(form_data, form_to_check, tax_form_info)
+        all_form_years = collect_tax_years(form_data, form_to_check, tax_form_info)
+        dict_with_form_data = get_min_max_years(all_form_years, dict_for_data, tax_form_info)
+        tax_form_info = append_to_main_list_as_json(dict_with_form_data, tax_form_info)
+        print(tax_form_info, "hi")
 
-    print(json.dumps(json_tax_forms, indent = 4))
+    print(json.dumps(tax_form_info, indent = 4))
     
     return print("All forms have been checked")
 
 
-get_page_html(tax_forms_to_check)
+get_tax_info(tax_forms_to_check)
