2d7a27122d8bc151194199a625892916be2ce4dc
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-10-28 20:51:17 +0000

    Pagination and download work

--- another_practice.py
+++ another_practice.py
@@ -1,10 +1,8 @@
-# start by only pulling the product num and title, then returning JSON
-
 import requests
 import os
 from bs4 import BeautifulSoup
 
-# TODO: fix infinite loop pagination; add mk dir and download files
+
 #pseudocode
 # make a list of the years' links needed for pdf dl's
 # if pdf found, add link to a list
@@ -22,15 +20,15 @@ from bs4 import BeautifulSoup
 
 
 tax_form_name = "Form W-2"
-start_year = 2018
-end_year = 2020
+start_year = 1954
+end_year = 1956
 
 
 def get_url(tax_form_name):
     form_name = tax_form_name.lower()
 
     url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=" + form_name + "&criteria=formNumber&resultsPerPage=25&isDescending=false"
-
+    
     return url
 
 
@@ -49,12 +47,12 @@ def get_pdf_links(soup, start_year, end_year):
     links = soup.find_all('a')
     for i in range(start_year, end_year+1):
         list_of_form_years.append(i)
-
     
         for link in links:
             link_url = link["href"]
             if 'pdf' in link_url and str(i) in link_url:
                 list_of_pdf_links.append(link_url)
+
     return list_of_pdf_links, list_of_form_years
 
 def make_subdirectory_for_pdfs():
@@ -81,9 +79,9 @@ def save_pdf(subdirectory_for_pdfs, list_of_pdf_links, list_of_pdf_years):
         r = requests.get(url) 
         print(url)
         print()
-        # with open(complete_name, 'wb') as f:
-        #     f.write(r.content)
-        #     f.close()
+        with open(complete_name, 'wb') as f:
+            f.write(r.content)
+            f.close()
 
     print("completed downloads")
 
@@ -101,7 +99,7 @@ def check_if_next_page(soup):
             new_url = url + item['href']
 
             return new_url
-    
+            
     return None
 
 
@@ -120,25 +118,13 @@ def get_downloads(url, start_year, end_year):
     list_of_pdf_links = list_of_forms[0]
     list_of_pdf_years = list_of_forms[1]
 
-    if list_of_forms == []:
-        print("list is empty!")
+    # check if pagination is necessary
+    if len(list_of_pdf_years) != len(list_of_pdf_links):
+        if_next = check_if_next_page(soup)
+        get_downloads(if_next, start_year, end_year)
     else:
         subdirectory_for_pdfs = make_subdirectory_for_pdfs()
         done = save_pdf(subdirectory_for_pdfs, list_of_pdf_links, list_of_pdf_years)
-
-    # check if pagination is necessary
-    if len(list_of_pdf_years) != len(list_of_pdf_links):
-        print("need to paginate")
-
-        # if_next = check_if_next_page(soup)
-        # if if_next != None:
-        #     print("link for if_next, exiting main function")
-        #     # get_downloads(url, start_year, end_year)
-        # else:
-        #     return list_of_forms
-
-    # print(list_of_pdf_links)
-
 
 
 print(download_pdfs_and_save(tax_form_name))
